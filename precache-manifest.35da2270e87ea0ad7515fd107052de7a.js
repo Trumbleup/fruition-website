226f3759c28c592db56fa6da8e5955e7adb38e78
Danny Trumble <dtrumble12@gmail.com>
2018-11-05 21:20:35 -0600

    Updates

---
+++ precache-manifest.35da2270e87ea0ad7515fd107052de7a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "4383f316d9c0bd45fa1fc00369a22cf6",
+    "url": "/fruition-website/static/media/guardian-zone.4383f316.png"
+  },
+  {
+    "revision": "ca56c87c0252300a9f5f",
+    "url": "/fruition-website/static/css/main.f935f8c9.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fruition-website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "6a3053c87668cd2e6c81",
+    "url": "/fruition-website/static/js/1.6a3053c8.chunk.js"
+  },
+  {
+    "revision": "387e02da280375f53477",
+    "url": "/fruition-website/static/js/runtime~main.387e02da.js"
+  },
+  {
+    "revision": "a073f3558c058278b785691983c52083",
+    "url": "/fruition-website/static/media/fruition-logo.a073f355.png"
+  },
+  {
+    "revision": "ab724734c6e31f1defcdb8a630ba4476",
+    "url": "/fruition-website/static/media/computer.ab724734.mp4"
+  },
+  {
+    "revision": "0381b488e0f3a72933c255da667b06b9",
+    "url": "/fruition-website/static/media/from-the-sun-logo.0381b488.png"
+  },
+  {
+    "revision": "47509ca1b149342ab68aaf2a3a04d36e",
+    "url": "/fruition-website/static/media/dezphonics.47509ca1.png"
+  },
+  {
+    "revision": "b7e224fb9d7d4397c80758bcecf2f0ab",
+    "url": "/fruition-website/static/media/my-crop-logo.b7e224fb.png"
+  },
+  {
+    "revision": "cbd81da38aba428d9370787ee80000fb",
+    "url": "/fruition-website/static/media/clear-key.cbd81da3.png"
+  },
+  {
+    "revision": "ca56c87c0252300a9f5f",
+    "url": "/fruition-website/static/js/main.ca56c87c.chunk.js"
+  },
+  {
+    "revision": "24b4babcb2339f9be12d99ffc6f48b19",
+    "url": "/fruition-website/static/media/j-sleeve.24b4babc.png"
+  },
+  {
+    "revision": "734f029532f8d637c8f74af11157317d",
+    "url": "/fruition-website/static/media/fruition-bulb.734f0295.png"
+  },
+  {
+    "revision": "949e1a9112dd3a6f86d63008564533a0",
+    "url": "/fruition-website/static/media/swangreen.949e1a91.png"
+  },
+  {
+    "revision": "15bd3661b1a53ef7b547ca8a70053e13",
+    "url": "/fruition-website/static/media/dark-bulb-logo.15bd3661.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fruition-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fruition-website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fruition-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fruition-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "6a3053c87668cd2e6c81",
+    "url": "/fruition-website/static/css/1.05ad42df.chunk.css"
+  },
+  {
+    "revision": "0753ecbec249a6f68c66454fe20c834d",
+    "url": "/fruition-website/index.html"
+  }
+];
